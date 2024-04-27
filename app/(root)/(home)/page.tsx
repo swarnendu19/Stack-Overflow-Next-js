e94a36ff36b9537609e11ace27577b24b3010263
Swarnendu <swarnendumaity633@gmail.com>
2024-04-27 18:53:34 +0530

    Next js routing

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="h1-bold">
+        Hey Hi  
+      </h1>
+    </div>
+  );
+}
---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AskQuestion = () => {
+  return (
+    <div>
+      Ask Question
+    </div>
+  )
+}
+
+export default AskQuestion
