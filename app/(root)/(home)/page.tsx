3fb46c35bd1088c1cb234041bfe9583e0cd836ad
Swarnendu <swarnendumaity633@gmail.com>
2024-05-19 20:33:23 +0530

    Local Seach Bar

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,5 @@
+import LocalSearchBar from "@/components/search/LocalSearchBar";
 import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 
 export default function Home() {
@@ -8,26 +8,25 @@ export default function Home() {
       {/* //Make h1 and Wrap with div "All Questions" */}
 
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
-       <h1 className="h1-bold text-dark100_light900">
-        All Questions
-       </h1>
-      </div>
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1> 
 
-      {/* // Below div make Button of Ask A Question linking to link /ask-question */}
-
-      <Link
-      href = "/ask-question"
-      className="flex justify-end max-sm:w-full"
-      >
-        <Button className="primary-gradient min-h-[46px]
-        px-4 py-3 !text-light-900">
-          Ask Question
-        </Button>
-
-      </Link>
-
-
-
+        <Link href="/ask-question" className="flex justify-end max-sm:w-full">
+           <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+            Ask a Question
+          </Button>
+        </Link> 
+      </div> 
+       
+       <div className="mt-11 flex justify-between gap-5
+       max-sm:flex-col sm:items-center">
+        <LocalSearchBar
+         route = "/"
+         iconPosition = "left"
+         imgSrc = "/assets/icons/search.svg"
+         placeholder  = "Search for Question"
+         otherClasses = "flex-1"
+        />
+       </div>
     </>
   )
 }
